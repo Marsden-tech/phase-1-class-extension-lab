dd69499683c0605475728a87945d90355cb07241
Marsden Mathews Odhiambo <marsdenlwero@gmail.com>
2024-08-07 14:35:10 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+class Polygon {
+    constructor(arr) {
+        this.arr = arr;
+    }
+    get countSides() {
+        return this.arr.length;
+    }
+    get perimeter() {
+        return this.arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        const [l, w, h] = this.arr;
+        return l + w > h && w + h > l && l + h > w;
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        return this.arr.every(side => side === this.arr[0])
+    }
+    get area() {
+        return this.arr[0] * this.arr[0]
+
+    }
+
+}
